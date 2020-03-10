e45d3d190b84411fcdd96b2217b8442cd370761b
金淼淋 <32184005+lindong629@users.noreply.github.com>
2020-03-10 20:06:37 +0800

    Update main.py
    V2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
 # Jimmy Lin
 
 import random
@@ -7,10 +7,15 @@ import random
 with open("num.txt","w",encoding="utf8") as f:
     for i in range(20):
         li = []
-        for i2 in range(5):
-            li.append(str(random.randrange(0, 10)))
-        result = "".join(li)
+        while True:
+            num = str(random.randint(0, 9))
+            if num not in li:
+                li.append(num)
+            if len(li) == 5:
+                result = "".join(li)
+                re1 = "".join(li[::-1])
+                break
         if i < 9:
-            f.write("0" + str(i + 1) + " " * 2 + "|" + " " * 2 + str(result) + " " * 2 + "[  ]" + "\n")
+            f.write("0" + str(i + 1) + " " * 2 + "|" + " " * 2 + str(result) + " | " + str(re1) + " " * 2 + "[  ]" + "\n")
         else:
-            f.write(str(i + 1) + " " * 2 + "|" + " " * 2 + str(result) + " " * 2 + "[  ]" + "\n")
+            f.write(str(i + 1) + " " * 2 + "|" + " " * 2 + str(result) + " | " + str(re1) + " " * 2 + "[  ]" + "\n")
